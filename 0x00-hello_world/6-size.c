2b39519747f9cfd252fae47aad55cbbf2d9840c6
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-10 14:04:53 -0700

    print size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ float num4;
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(num1));
 printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(num2));
-printf("Size of long long int: %lu byte(s)\n", (unsigned long)sizeof(num3));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(num3));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(num4));
 return (0);
 }
