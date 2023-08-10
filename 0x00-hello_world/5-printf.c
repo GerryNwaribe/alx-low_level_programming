5e3b380b12dcbf02378d2eb5fc2a4c39d39be288
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-10 09:02:18 -0700

    use printf to print string

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - print string using printf
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("\nwith proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
