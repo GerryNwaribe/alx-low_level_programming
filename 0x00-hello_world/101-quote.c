188b527176449152df70a669f0d9435e485bebb4
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-10 15:22:28 -0700

    unix operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,8 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	const char* message="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	size_t len=strlen(message);
+	write(1, message, len);
 	return (1);
 }
