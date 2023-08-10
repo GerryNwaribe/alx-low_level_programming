d9373bda3abb1bc9305fb87f0c2b8e45364bea4b
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-10 08:48:41 -0700

    use puts to print string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -print out string using outs
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle,");
+	return (0);
+}
