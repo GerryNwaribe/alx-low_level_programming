233d9431572fdeb0a9a2d232674b2d71e2ce1ee8
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-11 14:23:02 -0700

    all alphabets excpt q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - header
+ * main - print all alphabet except q and e
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+{
+	if (letter != 'q' && letter != 'e')
+	putchar(letter);
+	letter++;
+}
+	putchar('\n');
+	return (0);
+}
