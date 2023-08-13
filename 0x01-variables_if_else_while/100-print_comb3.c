014bffd5b2897bf02000fc8516c19c0311ca66fe
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-13 08:33:24 -0700

    diff combo of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * main - print different combo of two digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int firstNum = 0;
+
+	while (firstNum < 9)
+	{
+		int lastNum = 1;
+
+		while (lastNum <= 9)
+		{
+			putchar ('0' + firstNum);
+			putchar ('0' + lastNum);
+
+			if (firstNum != 8 || lastNum != 9)
+			{
+				putchar (',');
+			}	putchar (' ');
+			lastNum++;
+		}
+	firstNum++;
+	}
+
+	putchar ('\n');
+	return (0);
+}
