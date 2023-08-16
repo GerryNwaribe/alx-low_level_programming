78293ca9d4b06ea8626b3f4a5eb8802a6d2502e4
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-16 12:52:45 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,41 +1,43 @@
-#include <stdio.h>
-#include "main.h"
+#include<stdio.h>
+#include"main.h"
 /**
- * times_table - print zero to ten times table
- *
- * Return: 0
- */
-void times_table(void) {
-	int rows, col;
+* times_table - print zero to ten times table
+*
+* Return: 0
+*/
+void times_table(void)
+{
+int rows, col, product;
 
-	for (rows = 0; rows <= 9; rows++)
-	{
+for (rows = 0; rows <= 9; rows++)
+{
 
-	for (col = 0; col <= 9; col++)
-	{
-		int product = rows * col;
+for (col = 0; col <= 9; col++)
+{
+product = rows * col;
+if (col == 0)
+{
+putchar (product + '0');
 
-		if (product == 0 )
-		{
-		_putchar ('0');
-		}
-		else if (product < 10)
-		{
-		_putchar ('0' + (product % 10));
-		}
-		else
-		{
-			_putchar ('0' + (product / 10));
-			_putchar ('0' + (product % 10));
-		}
+}
 
-		if (col != 9)
-		{
-		_putchar (',');
-		_putchar (' ');
-		}
-	}
-	_putchar('\n');
-	}
+if (product < 10 && col != 0)
+{
+putchar (',');
+putchar (' ');
+putchar (' ');
+putchar (product + '0');
+}
+else if (product >= 10)
+{
+putchar (',');
+putchar (' ');
+putchar ('0' + (product / 10));
+putchar ('0' + (product % 10));
+}
 
+}
+
+putchar('\n');
+}
 }
