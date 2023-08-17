a0562df50cf4422938280640abbb4cbb52738991
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-17 15:02:05 -0700

    draw straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - print straight line
+ * @n: integer
+ *
+ * Return: 0
+ */
+void print_line(int n)
+{
+	if (n < 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		int m;
+
+		for (m = 0; m < n; m++)
+		{
+			_putchar ('_');
+		}
+		_putchar ('\n');
+	}
+}
