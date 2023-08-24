f5dcc57c9edcfa04947843795021c1f0c9d11b92
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-24 06:21:31 -0700

    copy string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncpy - prototype to copy string
+ * @dest: destination of string
+ * @src: source to be copied
+ * @n: character limitation
+ * Return: 0
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int lenght = 0;
+
+	while (dest[lenght] != '\0')
+	{
+		lenght++;
+	}
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+
+	}
+
+	return (dest);
+}
