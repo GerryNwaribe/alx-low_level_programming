8371e051727ecd3c542dfd138744dfec33699d1d
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-08-30 17:11:38 +0100

    print string with newline

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,17 @@
+#ifndef main_h
+#define main_h
+/**
+ * main - Entry point
+ * description - headers
+ *
+ * Return: 0
+ */
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _putchar(char a);
+#endif
