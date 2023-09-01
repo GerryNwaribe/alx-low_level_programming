35e047d81b5e4addd49c9437f3d769f95ec5d423
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-01 09:49:48 -0700

    create static library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -7,5 +7,30 @@
  */
 int _atoi(char *s)
 {
-return (0);
+	/* Initialize sign to positive and index i to 0. */
+	int sign = 1, i = 0;
+	/* Initialize the result variable to 0. */
+	unsigned int res = 0;
+
+	/* First, skip any non-digit characters and handle the sign. */
+	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
+	{
+		if (s[i] == '-')
+			/* Change the sign to negative if a '-' character is encountered. */
+			sign *= -1;
+		i++;
+	}
+
+	/* Now, parse the digits and calculate the integer value. */
+	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
+	{       /* Calculate the integer value digit by digit. */
+		res = (res * 10) + (s[i] - '0');
+		i++;
+	}
+
+	/* Multiply the result by the sign to account for the sign of the integer. */
+	res *= sign;
+
+	/* Return the resulting integer value. */
+	return (res);
 }
