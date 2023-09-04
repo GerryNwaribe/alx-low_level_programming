fc91c3ac1f6e2e8261e936633b5d3748aef2e9c5
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-04 13:36:35 +0100

    created a prototype for array of chars

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * create_array - prototype to create array of chars
+ * @size: size of array
+ * @c: specific char to be specailized with
+ * Return: 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p = malloc(size * sizeof(char));
+	unsigned int a;
+
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+
+
+	for (a = 0; a < size; a++)
+	{
+		p[a] = c;
+	}
+	return (p);
+}
