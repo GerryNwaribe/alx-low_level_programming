04bb8bfa40d857498cb660d30720f00b8b2eab6a
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-04 15:07:27 +0100

    point to memory of copied string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _strdup - prototype to copy a string a point to new allocated memory
+ * @str: string to be copied
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	int size = 0;
+	int b;
+	char *a;
+
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+
+	a = malloc((size + 1) * sizeof(char));
+
+	if (*str == '\0')
+	{
+		return (NULL);
+	}
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+
+	for (b = 0; b <= size; b++)
+	{
+		a[b] = str[b];
+	}
+	return (a);
+	free(a);
+
+}
