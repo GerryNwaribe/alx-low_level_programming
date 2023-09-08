6be25bb3d7aac0aba150f2bf03c5ca54eef343d8
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-08 10:20:06 +0100

    concatenate 2 strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,11 +17,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int copy_len;
 	char *c;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
 	{
-		s1 = " ";
-		s2 = " ";
-
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	if (s1 != NULL)
