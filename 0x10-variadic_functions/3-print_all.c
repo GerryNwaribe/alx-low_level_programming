8297385efccbaaec01f48d305a7b45359a5fe49b
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-14 23:37:20 +0100

    requirement change

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,51 +9,51 @@
 void print_all(const char * const format, ...)
 {
 	char *str;
-    int a = 0;
-    va_list quarrel;
-    int b = 0;
-    va_start(quarrel, format);
-if (format)
-{
-    while (format[a] != '\0')
-    {
-        switch (format[a])
-        {
-            case 'c':
-                printf("%c", va_arg(quarrel, int));
-				b = 1;
-                break;
-            case 'i':
-                printf("%d", va_arg(quarrel, int));
-				b = 1;
-                break;
-            case 'f':
-                printf("%f", va_arg(quarrel, double));
-				b = 1;
-                break;
-            case 's':
-               {
-                str = va_arg(quarrel, char *);
-                if (str == NULL)
-                {
-                    printf("(nil)");
-                    break;
-                }
-                printf("%s", str);
-				b = 1;
-                break;
-               }	
-            	default:
-                break;
-        }
-		if (format[a + 1] != '\0' && b) 
-		printf(", ");
-		a++;
-		b = 0;
-        
-    }
-}
-    printf("\n");
-    va_end(quarrel);
+	int a = 0;
+	va_list quarrel;
+	int b = 0;
+	va_start(quarrel, format);
+	if (format)
+	{
+		while (format[a] != '\0')
+		{
+			switch (format[a])
+			{
+				case 'c':
+					printf("%c", va_arg(quarrel, int));
+					b = 1;
+					break;
+				case 'i':
+					printf("%d", va_arg(quarrel, int));
+					b = 1;
+					break;
+				case 'f':
+					printf("%f", va_arg(quarrel, double));
+					b = 1;
+					break;
+				case 's':
+					{
+						str = va_arg(quarrel, char *);
+						if (str == NULL)
+						{
+							printf("(nil)");
+							break;
+						}
+						printf("%s", str);
+						b = 1;
+						break;
+					}
+				default:
+					break;
+			}
+			if (format[a + 1] != '\0' && b)
+				printf(", ");
+			a++;
+			b = 0;
+
+		}
+	}
+	printf("\n");
+	va_end(quarrel);
 }
 
