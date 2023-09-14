5a20d4bc2ad36842dd96e318a172e3a1cfd7e4fb
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-14 17:03:32 +0100

    modified if statement

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,25 +7,27 @@
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-    char *b;
-    unsigned int a = n;
-    va_list quarrel;
-    
-    va_start(quarrel, n);
-    while (a > 0)
-    {
-        b = va_arg(quarrel, char *);
-        if (separator != NULL && a != 1)
-        {
-            printf("%s", b);
-            printf("%s", separator);
-        }
-        else
-        {
-            printf("%s", b);
-        }
-        a--;
-    }
-    printf("\n");
-    va_end(quarrel);
+	char *b;
+	unsigned int a = n;
+	va_list quarrel;
+
+	va_start(quarrel, n);
+	while (a > 0)
+	{
+		b = va_arg(quarrel, char *);
+		if (b != NULL)
+		{
+			printf("%s", b);
+			printf("%s", separator);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		a--;
+	}
+	printf("\n");
+	va_end(quarrel);
 }
+
