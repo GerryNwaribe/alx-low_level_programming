09c8c0385e2173f772c87e1cb8a4e1fd31aca053
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-21 23:34:47 +0100

    function to print before main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ * print_before_main - function to print before main
+ * Return: 0
+*/
+void print_before_main()
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
