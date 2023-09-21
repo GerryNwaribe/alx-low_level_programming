5c60cbfcda405d7c925c2ddd4e8861be5afc52b9
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-21 23:38:26 +0100

    bettied files

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,8 +2,9 @@
 /**
  * print_before_main - function to print before main
  * Return: 0
-*/
-void print_before_main()
+ */
+void print_before_main(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+			I bore my house upon my back!\n");
 }
