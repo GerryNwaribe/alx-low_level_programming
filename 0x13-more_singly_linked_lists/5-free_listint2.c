4581b4c5f919ee342c5dc3ab72c05cf6a3276afe
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-26 12:25:34 +0100

    changed condition

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,10 +7,7 @@
 void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
-	listint_t *new_node;
 
-	if (*head == NULL)
-		*head = new_node;
 	while (*head != NULL)
 	{
 		current_node = *head;
