576f5bd50472de9abf90dc52f525c9ee26f5a481
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-28 00:14:06 +0100

    function that frees list (safe)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * free_listint_safe -  function that frees a listint_t list
+ * @h: pointer
+ * Return: 0
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t node_count = 0; /* Initialize a counter for the number of nodes */
+	listint_t *current = *h; /* Create a current pointer for traversal */
+	listint_t *next_node = NULL; /* Create a pointer to the next node */
+
+	while (current != NULL)
+	{
+		node_count++; /* Increment the node count */
+		next_node = current->next; /* Store the next node pointer */
+
+		/* Free the current node */
+		free(current);
+
+		/* Move to the next node */
+		current = next_node;
+	}
+
+	*h = NULL; /* Set the head to NULL */
+
+	return (node_count); /* Return the number of nodes that were freed */
+}
