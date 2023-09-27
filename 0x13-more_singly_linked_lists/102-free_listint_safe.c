8b12a8a1f71d69a6f03ead951898a427ba0039e2
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-28 00:21:50 +0100

    accommodated for nil

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -15,6 +15,12 @@ size_t free_listint_safe(listint_t **h)
 		node_count++; /* Increment the node count */
 		next_node = current->next; /* Store the next node pointer */
 
+		/* Check if the next node is NULL and print (nil) */
+		if (!next_node)
+		{
+			printf("[%p] %s\n", (void *)next_node, "(nil)");
+		}
+
 		/* Free the current node */
 		free(current);
 
@@ -24,5 +30,5 @@ size_t free_listint_safe(listint_t **h)
 
 	*h = NULL; /* Set the head to NULL */
 
-	return (node_count); /* Return the number of nodes that were freed */
+	return node_count; /* Return the number of nodes that were freed */
 }
