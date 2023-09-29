58f75d9eedb02fca0f60eba800b8b5e2eb880225
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-09-29 02:42:53 +0100

    function to clear bits

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * clear_bit - function that sets the value of a bit to 1
+ * @n:nmber to be set
+ * @index: index
+ * Return: 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	unsigned long int bitmask = ~1UL << index;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	*n &= bitmask;
+
+	return (1);
+}
