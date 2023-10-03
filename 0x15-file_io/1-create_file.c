cd3e2657ea21777448526f23650a40a6985ec122
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-10-03 01:15:40 +0100

    function to create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * create_file - Create or overwrite a file with the specified content.
+ * @filename: The name of the file to create or overwrite.
+ * @text_content: The content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, written, content;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		content = 0;
+		while (text_content[content])
+		{
+			content++;
+		}
+	}
+
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	written = write(file, text_content, content);
+
+	if (file == -1 || written == -1)
+	{
+		return (-1);
+	}
+
+	close(file);
+	return (1);
+}
