fbfd39f290238f78fe890543f66de8b2d8f86109
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-10-03 01:19:19 +0100

    changed while loop to for

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,12 +14,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		content = 0;
-		while (text_content[content])
-		{
-			content++;
-            continue;
-		}
+		for (content = 0; text_content[content]; content++)
+			continue;
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
