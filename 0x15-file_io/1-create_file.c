cea3fccc05886768f744f232d715b295d3ac740e
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-10-03 01:23:00 +0100

    changed loop again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,8 +14,12 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (content = 0; text_content[content]; content++)
-			continue;
+		content = 0;
+		while (text_content[content])
+		{
+			content++;
+		}
+		/*for (content = 0; text_content[content]; content++) continue;*/
 	}
 
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
