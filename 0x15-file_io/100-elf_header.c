bb322c43bdd9e7315c41ba61b2276752a2606f25
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-10-20 23:45:57 +0100

    elf header file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,73 @@
 #include "main.h"
 /**
- * 
- * 
- * 
-*/
+ * print_elf_header_info - isplays the info in the ELF header
+ * @header: pointer to Elf64_Ehdr *header
+ * Return: 0
+ */
+void print_elf_header_info(Elf64_Ehdr *header)
+{
+	int i;
+
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+	{
+		printf("%02x ");
+	}
+	printf("\n");
+
+	printf("  Class:                             %s\n", header->e_ident[EI_CLASS] == ELFCLASS32 ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n", header->e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "2's complement, big-endian");
+	printf("  Version:                           %d (current)\n", header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n", header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n", header->e_ident[EI_ABIVERSION]);
+	printf("  Type:                              %s\n", header->e_type == ET_EXEC ? "EXEC (Executable file)" : "UNKNOWN");
+	printf("  Entry point address:               0x%lx\n", header->e_entry);
+}
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+	Elf64_Ehdr header;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
+		return (98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		fprintf(stderr, "Error: Cannot open file '%s'\n", argv[1]);
+		return (98);
+	}
+
+
+
+	if (read(fd, &header, sizeof(header)) != sizeof(header))
+	{
+		fprintf(stderr, "Error: Cannot read ELF header\n");
+		close(fd);
+		return (98);
+	}
+
+
+	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+			header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
+	{
+		fprintf(stderr, "Error: Not an ELF file\n");
+		close(fd);
+		return (98);
+	}
+
+	print_elf_header_info(&header);
+
+	close(fd);
+	return (0);
+}
