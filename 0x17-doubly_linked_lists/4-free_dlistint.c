9ab0de2c3c7b33a108c3fa5e9e2166da5fdada53
GerryNwaribe <the.star.is.geraldine@gmail.com>
2023-11-13 23:22:27 +0100

    function to free nodes

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: pointer to head of list
+ * Return: nothing, frees all nodes
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *current_node, *next_node;
+
+	current_node = head;
+
+	while (current_node != NULL)
+	{
+		next_node = current_node->next;
+		free(current_node);
+		current_node = next_node;
+	}
+}
